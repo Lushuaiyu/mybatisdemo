54019a0e8b8ec5ba1faa033df164e0bfffd39989
Lushuaiyu <lsy1246796812@gmail.com>
2019-05-13 22:46:26 +0800

    swagger

--- src/main/java/com/or/mybatisdemo/controller/UserController.java
+++ src/main/java/com/or/mybatisdemo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.or.mybatisdemo.controller;
 import com.or.mybatisdemo.domain.eneity.User;
 import com.or.mybatisdemo.query.QueryUser;
 import com.or.mybatisdemo.service.api.UserService;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +21,17 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("user")
+@Api(value = "测试接口controller")
+
 public class UserController {
     @Resource
     private UserService userService;
+
+    @ApiOperation(value = "测试用接口", notes = "测试用接口", httpMethod = "POST")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", value = "起始页", dataType = "string", required=true, paramType="form"),
+            @ApiImplicitParam(name = "limit", value = "每页多少条数据", dataType = "string", required=true, paramType="form")
+    })
     @RequestMapping("queryParams")
     public List<User> queryParams(@RequestParam(defaultValue = "1") String page,
                                   @RequestParam(defaultValue = "5") String limit,
